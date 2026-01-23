b7dee57dc31a3636b6cb591aaf8a9b53683f777f
l1N322 <Pro100.dandi@yandex.ru>
2026-01-23 22:05:47 +0300

    Изменённое задание 3

--- Задание 3.py
+++ Задание 3.py
@@ -3,18 +3,22 @@
 
 def flatten(data):
     """
-    Рекурсивно преобразует вложенный список в плоский список.
+    Рекурсивно преобразует вложенные списки, кортежи и множества в плоский список.
     """
     result = []
 
     for element in data:
-        if type(element) == list:
+        if isinstance(element, (list, tuple, set)):
             result.extend(flatten(element))
         else:
             result.append(element)
 
     return result
 
+
 if __name__ == "__main__":
     print(flatten([1, [2, [3, 4], 5]]))
     print(flatten([0, [1, 2, [3, [4], 5], 6]]))
+    print(flatten([1, (2, 3), [4, 5]]))
+    print(flatten([1, {2, 3}, (4, [5, 6])]))
+    print(flatten((1, [2, 3], {4, 5, 6})))
